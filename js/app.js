cb797f620a934b67757dca5235bc5cd93a496ae1
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-05-18 12:36:29 +0100

    messing with head

--- js/app.js
+++ js/app.js
@@ -6,6 +6,30 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return App.Avatar.create({head : App.HeadsController.get('firstObject')})
+
   }
+});
+
+App.Avatar = Ember.Object.extend({
+    name : 'test',
+    hat: null,
+    body : null,
+    legs : null
+
+
+});
+
+App.BodyPart = Ember.Object.extend({
+    type : null,
+    image: function() {
+        return 'images/' + this.get('type') + this.get('id') + '.png';
+    }.property('type','id'),
+
+});
+
+App.HeadsController = Ember.ArrayController.create({
+    content: [App.BodyPart.create({type:'head',id:1})]
+
+
 });
